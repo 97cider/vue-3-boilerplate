8cb6bfd9fd006946f16ac3d1cd5fe73d3773b739
97Cider <smcfadden150@gmail.com>
2022-07-27 14:19:24 -0400

    Add Babel Support

--- webpack.common.js
+++ webpack.common.js
@@ -18,10 +18,14 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-transform-runtime']
+          }
         },
       },
       {
